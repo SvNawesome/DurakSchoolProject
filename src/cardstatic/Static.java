9db386ee62a9edff04053bea37a67e46aae9efe1
suNNsh1ne <h4cker1@web.de>
2015-04-22 13:29:54 +0200

    Static for Cards finished
    +Added Card Values + Suits
    
    +Added Hashmaps
    -ranks and suits are the keys
    -to give the ranks - Values
    -and to give the suits colors.
    
    +added for-loop to test the Hashmaps
    
    -removed //ToDo comments

--- src/cardstatic/Static.java
+++ src/cardstatic/Static.java
@@ -1,16 +1,84 @@
 package cardstatic;
+import java.io.*;
+import java.util.*;
 
 public class Static {
-	// Kartenname
-		// ToDo
-		
-	// Kartenwerte
-		// ToDo
 	
-	// Karten Symbole Herz, Karo, Pik, Kreuz
-		// ToDo
+	// Kartenname in ein Array speichern
+	
+	public static final String[] ranks =
+		{
+		"6",
+		"7",
+		"8",
+		"9",
+		"10",
+		"Jack",
+		"Queen",
+		"King",
+		"Ace"
+		};
+
+	
+	// Kartenwerte
+	// Hashmap Erstellen (Key + Value) mit der For-Schleife das Array mit den Namen durchgehen und es als Key setzen
+	// und jedem Key einen Wert zu weisen um Damit später einfachere vergleiche durchzuführen
+	
+	public static void cardValues()
+	{
+		Map<String, Integer> cardMap = new HashMap<String, Integer>();
+		for(int i = 6;i < 15;i++)
+		{
+			cardMap.put(ranks[i-6], i);
+		}
+		
+		//zum Testen wird die Map durchgegangen für jeden gesetzen Key
+		for(String key: cardMap.keySet())
+		{
+			System.out.print("Key: " + key + " - ");
+			System.out.println("Value: " + cardMap.get(key) + "\n");
+		}
+
+	}
+	
+	// Karten Symbole Herz, Karo, Pik, Kreuz in Array speichern
+	public static final String[] suits =
+		{
+			"Spades",
+			"Clubs",
+			"Hearts",
+			"Diamonds"
+		};
 		
 	// Karten Farbe
-		// ToDo
+	// Karten Symbole (als Key), den passenden Farben zuordnen
+	public static void suitColors()
+	{
+		Map<String, String> colorMap = new HashMap<String, String>();
+			colorMap.put("Spades", "black");
+			colorMap.put("Clubs", "black");
+			colorMap.put("Hearts", "red");
+			colorMap.put("Diamonds", "red");
+			
+			
+		// Zum Testen der Zuordnung
+		for(String key: colorMap.keySet())
+		{
+			System.out.print("Key: " + key + " - ");
+			System.out.println("Value: " + colorMap.get(key) + "\n");
+		}
+	}
 
+	
+	//zum Testen
+	/*
+	 
+	public static void main(String[] argv)
+	{
+		Static.cardValues();
+		Static.suitColors();
+	}
+	
+	*/
 }
+
