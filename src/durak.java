c9acf36ad980d06fb07f44b5af6f4c89064c97be
Awesome327 <Awesome327@users.noreply.github.com>
2015-05-07 11:21:56 +0200

    Update durak.java

--- src/durak.java
+++ src/durak.java
@@ -1,12 +1,57 @@
+
 public class durak {
-	// Spiellogik
+	
+ //	private ;
+	
+	void setAttacker(Player PlayerID)
+	{
+		PlayerID = 1;
+	}
+	
+    int getAttacker(Player PlayerID)
+    {
+    	for(int i = 0; i < PlayerID.length;i++)
+		{
+			if (PlayerID[i]==0)
+			{
+				System.out.println(PlayerID[i]);
+				System.out.println(i);
+			}
+		}
+    }
+    
+    void setDefender(int currentPlayer)
+	{
+		currentPlayer = 0;
+	}
+	
+    int getDefender()
+    {
+       return currentPlayer;	
+    }
+    
+    public static final void changeCurrentPlayer()
+    {
+    	if (currentPlayer = 1)
+    	{
+    		setDefender(currentPlayer);
+    	}
+    	
+    }
+	
+		
+	}
+	
+// Spiellogik
 	
 	// Karten vergleichen (aufrufen)
 	
 	// Angreifer bestimmen
 	
+	
+	
 	// Verteidiger bestimmen
 	
-	// Verteidigung durchführen (vergleich je 2er paare)
+	// Verteidigung durchfÃ¼hren (vergleich je 2er paare)
 
 }
