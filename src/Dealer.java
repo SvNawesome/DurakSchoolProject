cc4ffc630679f699543229309e56e5820417fa9a
finn31 <finn.bohn@web.de>
2015-05-21 14:50:01 +0200

    setter + getter
    created setter + getter for Deck
    fixed remove
    fixed errors

--- src/Dealer.java
+++ src/Dealer.java
@@ -7,13 +7,8 @@ public class Dealer {
 
 	Deck dealerDeck;
 	
-	public Dealer(){
-		dealerDeck.getDeck();
-		//Deck dealerDeck = new Deck();
-	}
-	
-	public void createNewDeck(){
-		dealerDeck.createNewDeck();
+	public Dealer(Deck deck){
+		dealerDeck = deck.getDeck();
 	}
 	
 	public void dealCards(ArrayList<Player> Players){
@@ -25,8 +20,12 @@ public class Dealer {
 	
 	public void trumpDraw(){
 		dealerDeck.setTrump(dealerDeck.topCardDraw().suit);
-		dealerDeck.addCard(dealerDeck.getDeck(), dealerDeck.topCardDraw());
-		dealerDeck.removeCard(dealerDeck.getDeck(), dealerDeck.topCardDraw());
 	}
 
+
+	public static void main(String[] args){
+		Deck deck = new Deck();
+		Dealer dealer = new Dealer(deck);
+		dealer.trumpDraw();
+	}
 }
--- src/Deck.java
+++ src/Deck.java
@@ -4,11 +4,12 @@ import java.util.*;
 import cardAssignment.*;
 
 public class Deck {
+	Deck deck;
 	String Trump;
-	ArrayList<Card> deck = new ArrayList<Card>();
 	
 	public Deck()
 	{
+		ArrayList<Card> deck = new ArrayList<Card>();
 		for (String rank : Assignment.ranks) {
 			for (String suit : Assignment.suits) {
 				deck.add(new Card(rank, suit));
@@ -22,14 +23,24 @@ public class Deck {
 		return(newDeck);
 	}
 	
+	public Card getCard() {
+		//!
+		return null;
+	}
+	
 	public Card topCardDraw()
 	{
 		Card a = new Card("King", "Hearts");
-		//a = deck(0);
-		deck.remove(0);
+		a = this.getCard();
+		this.remove(0);
 		return a;
 	}
-	
+
+	private void remove(int i) {
+		// TODO Auto-generated method stub
+		this.remove(i);
+	}
+
 	public String getTrump()
 	{
 		return Trump;
@@ -40,8 +51,12 @@ public class Deck {
 		this.Trump = Trump;
 	}
 	
-	public ArrayList<Card> getDeck(){
-		return this.deck;
+	public void setDeck(Deck setDeck){
+		deck = setDeck;
+	}
+	
+	public Deck getDeck(){
+			return deck;
 	}
 	
 	public void addCard(ArrayList<Card> deck, Card card){
@@ -51,4 +66,10 @@ public class Deck {
 	public void removeCard(ArrayList<Card> deck, Card card){
 		deck.remove(card);
 	}
+	
+	public static void main(String[] args){
+		//h
+	}
 }
+
+	
