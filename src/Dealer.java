cf035c9ab91d297b628143a4ea594b72bf85ff45
finn31 <finn.bohn@web.de>
2015-05-28 10:43:45 +0200

    Kommentare
    Kommentare für dealer geschrieben

--- src/Dealer.java
+++ src/Dealer.java
@@ -2,11 +2,15 @@ import java.util.ArrayList;
 
 public class Dealer {
 
+	//Initialisieren des Trumpf Strings
 	private static String Trump;
+	
+	//Konstruktor gibt dem Dealer ein neues Deck
 	public Dealer(Deck deck){
 		deck.getDeck();
 	}
 	
+	//Karten an Spieler austeilen (6) und Trumpf ziehen aufrufen
 	public void dealCards(ArrayList<Player> Players, Deck deck){
 		for(int i = 0; i < Players.size(); i++){
 			System.out.println("\nSpieler" + i + " zieht:");
@@ -15,6 +19,7 @@ public class Dealer {
 		trumpDraw(deck);
 	}
 	
+	//Trumpf ziehen und die gezogene Karte wieder an das Deck anfügen
 	public void trumpDraw(Deck dealerDeck){
 		System.out.println("\nTrumpf gezogen");
 		Card tmpCard = dealerDeck.getCard();
